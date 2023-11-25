5fe35c7d4769bdb7a766b961e84eaf3dd689749e
md.jewel mia <md.jewelmia52@gmail.com>
2023-11-25 22:02:29 +0600

    initial commit and api generate

---
+++ api/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+console.log(process.env.POSTGRES_USER);
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      password: 'admin',
+      username: 'postgres',
+      entities: [],
+      database: 'linkedin',
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
